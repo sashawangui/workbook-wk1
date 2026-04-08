42063de0808e56394400569e222520635022e6e6
Sashawangui <sasha.iluku@gmail.com>
2026-04-08 15:48:08 -0700

    Done with exercise 2

---
+++ src/main/java/pluralsight/VariableApp.java
@@ -0,0 +1,47 @@
+package pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        System.out.println("Exercise 2");
+        System.out.println("Step 1");
+        System.out.println();
+
+
+        String favoriteColor = "Red";
+        System.out.println("My favorite color is " + favoriteColor);
+
+        int graduationYear = 2022;
+        System.out.println("I graduated in " + graduationYear);
+
+        char middleInitial = 'm';
+        System.out.println("My middle initial is " + "'" + middleInitial + "'");
+
+        boolean havePets = false;
+            if (havePets == false) {
+            System.out.println("I do not have pets");
+        }
+        String niceMessage = "If you don't know it yet, you will. Your confidence will turn into knowledge";
+        System.out.println("'" + niceMessage + "'");
+
+        System.out.println();
+        System.out.println("Step 2");
+
+        int days_per_week = 7;
+        System.out.println("There are " + days_per_week + " days in a week.");
+
+        double coffeePrice = 4.99;
+        System.out.println("The price of coffee is $" + coffeePrice);
+
+        char favoriteLetter = 'j';
+        System.out.println("My favorite letter is " + favoriteLetter);
+
+        boolean isRaining = false;
+            if (isRaining == false){
+                System.out.println("It is not raining");
+            }
+        System.out.println();
+        System.out.println("Step 3");
+    }
+}
+
+
